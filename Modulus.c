3d7e8621bff57a05decd8e56fe86dcc730724143
ShreerangManchali <shree5rang@gmail.com>
2024-10-27 03:08:52 +0530

    Add files via upload

---
+++ Modulus.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+int a = 5, b = 5;
+int result = a%b;
+printf("%d",result);
+return 0;
+}
+//This code shows the execution of an arethmatic operator "modulus", while using two fixed inputs and using another variable to store the final result
+//also printed using printf function
---
+++ Subtraction.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+int a = 2, b = 5;
+int result = a-b;
+printf("%d",result);
+return 0;
+}
+//This code shows the execution of an arethmatic operator "subtraction", while using two fixed inputs and using another variable to store the final result
+//also printed using printf function
