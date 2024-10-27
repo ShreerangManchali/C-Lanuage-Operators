8caa6eada883205179c0e35efaa6dbcf001e68c1
ShreerangManchali <shree5rang@gmail.com>
2024-10-27 21:50:57 +0530

    Add files via upload

---
+++ Increment.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main ()
+{
+int a= 5;
+a++;
+printf("%d",a);
+return 0;
+}
+//This code is made using two fixed inputs and prints the result of "Increment" operator using printf function
---
+++ NOTLogical.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main ()
+{
+int a= 5, b=7;
+printf("%d",a>0 | b>0);
+return 0;
+}
+//This code is made using two fixed inputs and prints the result of "NOT (Logical)" operator using printf function
