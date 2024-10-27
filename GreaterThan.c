526b2a64ba051bda7d213cb59ea6aee275be5b6f
ShreerangManchali <shree5rang@gmail.com>
2024-10-27 21:37:01 +0530

    Add files via upload

---
+++ GreaterThan.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main ()
+{
+int a= 7, b=5;
+printf("%d",a>b);
+return 0;
+}
+//This code is made using two fixed inputs and prints the result of "greater than" operator using printf function
---
+++ LessEqual.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main ()
+{
+int a= 5, b=7;
+printf("%d",a<=b);
+return 0;
+}
+//This code is made using two fixed inputs and prints the result of "lesser or equal than" operator using printf function
